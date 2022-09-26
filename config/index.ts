a1c85312aef470fa5eb3169ebbc4d428cdb12a4e
Shayan Shiravani <shayan5420@yahoo.com>
2022-09-26 15:49:24 +0330

    fix: Fix env variables

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,5 @@
 
-export const LIVEKIT_BACKEND_URL = process.env.LIVEKIT_BACKEND_URL || "http://localhost:5004/v1/"
+export const LIVEKIT_BACKEND_URL = process.env.NEXT_PUBLIC_LIVEKIT_BACKEND_URL || 
+"http://localhost:5004/v1/"
 
-export const LIVEKIT_SERVER_URL = process.env.LIVEKIT_SERVER_URL || "ws://localhost:7880"
+export const LIVEKIT_SERVER_URL = process.env.NEXT_PUBLIC_LIVEKIT_SERVER_URL || "ws://localhost:7880"
